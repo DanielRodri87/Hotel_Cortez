9a42c442e772fc8ab8c6dd11636913cdf716ae34
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2024-01-20 15:23:16 -0300

    organizacao menu

--- main.c
+++ main.c
@@ -18,13 +18,13 @@ int main(void) {
         printf("SEJA BEM-VINDO AO SISTEMA DO HOTEL CORTEZ\n");
         printf("=============================================\n");
 
-        printf("Informe qual operacao voce deseja usar: \n1 - Gerenciar Hotel\n2 - Gerenciar Clientes\n");
+        printf("Informe qual operacao voce deseja usar: \n1 - Gerenciar Hotel\n2 - Gerenciar Clientes\n3 - Sair: \n");
         scanf("%d", &opcao_menu1);
 
         switch (opcao_menu1) {
             case 1:
                 system("clear || cls");
-                printf("Seja bem-vindo ao gerenciamento do Hotel\nEscolha a op desejada:\n1 - Gerenciar Quartos\n2 - Gerenciar Finanças\n");
+                printf("Seja bem-vindo ao gerenciamento do Hotel\nEscolha a op desejada:\n1 - Gerenciar Quartos\n2 - Gerenciar Finanças\n 3 - Voltar\n");
                 scanf("%d", &opcao_menu_hotel);
                 switch (opcao_menu_hotel) {
                     case 1:
@@ -35,6 +35,8 @@ int main(void) {
                         financas();
                         break;
 
+                    case 3:
+                        printf("Voltando ao menu principal...");
                     default:
                         break;
                 }
@@ -43,6 +45,11 @@ int main(void) {
                 printf("Gerenciar Clientes \n");
                 cadastrar_cliente();
                 printf("Cliente hóspede cadastrado com sucesso!");
+                break;
+            
+            case 3:
+                printf("Saindo do sistema...");
+                exit(0);
                 break;
         }
     }
